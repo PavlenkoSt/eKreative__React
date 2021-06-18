127dcbbd7fbee41c2a42682b925d04ed1b88a7a3
Stanislav <verbatim8872@gmail.com>
2021-06-18 11:10:37 +0300

    prepare to development

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Route } from 'react-router-dom'
+import Login from './Screens/Login';
+
+const App = () => {
+  return (
+    <div>
+      <Route path='/login' component={Login}/>
+    </div>
+  )
+}
+
+export default App;
